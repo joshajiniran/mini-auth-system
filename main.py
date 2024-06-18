58ae25bc983e83769eb6f8acce86480893a0a192
Josh Ajiniran <josuajiniran@gmail.com>
2024-06-18 01:58:53 +0100

    mini auth system v1

---
+++ main.py
@@ -0,0 +1,66 @@
+user_base = [
+    {"username": "josh", "password": "12345678"},
+    {"username": "jane", "password": "87892434"},
+    {"username": "james", "password": "something"},
+]
+
+
+def login() -> None:
+    print("Login")
+    print("======================")
+    username: str = input("Enter your username: ")
+    password: str = input("Enter your password: ")
+
+    for user in user_base:
+        if username == user["username"]:
+            if password == user["password"]:
+                print("Logged in successfully.")
+                return
+            else:
+                print("Invalid password")
+                return
+
+    print("Invalid username")
+
+
+def register() -> None:
+    print("Register")
+    print("======================")
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    confirm_password = input("Confirm your password: ")
+
+    if password != confirm_password:
+        print("Password do not match")
+        return
+
+    user_base.append({"username": username, "password": password})
+
+    print(user_base)
+
+
+print("Welcome to Auth System")
+print("======================")
+
+print("1. Login")
+print("2. Register")
+print("3. Exit")
+
+try:
+    option: int = int(
+        input("Press the corresponding number (1, 2 or 3) to interact with system: ")
+    )
+except ValueError:
+    print("Invalid input, please enter a number not string")
+    exit(1)
+
+if option == 1:
+    login()
+elif option == 2:
+    register()
+elif option == 3:
+    print("Closing the application...")
+    exit(0)
+else:
+    print("Invalid input. Please enter one of the options (1, 2 or 3)")
+    exit(-1)
